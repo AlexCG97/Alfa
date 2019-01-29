b1ac3972533d4d82c829b1455aafdd8420b56c9d
AlexCG97 <ghiurcaalex@gmail.com>
2019-01-29 17:43:15 +0100

    primo caricamento

---
+++ login.php
@@ -0,0 +1,37 @@
+<?php
+   session_start();
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+
+</style>
+<title>Login - Alfa0</title>
+</head>
+<body>
+
+<h2>Login</h2>
+  <div class="imgcontainer">
+    <!--<img src="" alt="Avatar" class="avatar"> -->
+  </div>
+
+  <div class="container">
+    <form action="checkdata.php" method="post">
+    <label for="unsername"><b>Username</b></label><br>
+    <input type="text" placeholder="Inserisci L'Username" name="Username" required><br>
+
+    <label for="psw"><b>Password</b></label><br>
+    <input type="password" placeholder="Inserisci la Password" name="Password" required>
+        
+    <button type="submit">Login</button>
+  </form>
+  </div>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>
