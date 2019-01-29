089c36008a3c1a4935dce53cc1d1deab777275b5
AlexCG97 <ghiurcaalex@gmail.com>
2019-01-29 17:54:39 +0100

    //

--- login.php
+++ login.php
@@ -4,6 +4,7 @@
 <!DOCTYPE HTML>
 <html>
 <head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <meta charset="utf-8">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -15,19 +16,19 @@
 <body>
 
 <h2>Login</h2>
-  <div class="imgcontainer">
+  <div class="container">
     <!--<img src="" alt="Avatar" class="avatar"> -->
   </div>
 
-  <div class="container">
+    <div class="container">
     <form action="checkdata.php" method="post">
     <label for="unsername"><b>Username</b></label><br>
     <input type="text" placeholder="Inserisci L'Username" name="Username" required><br>
 
     <label for="psw"><b>Password</b></label><br>
-    <input type="password" placeholder="Inserisci la Password" name="Password" required>
-        
-    <button type="submit">Login</button>
+    <input type="password" placeholder="Inserisci la Password" name="Password" required><br>
+    <button type="submit" class="btn btn-success">Login</button>   
+
   </form>
   </div>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
